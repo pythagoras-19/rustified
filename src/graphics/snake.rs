5f8b3ed9a6d39fb198e64de1a5453eae979600fb
Matt Christiansen <mattchristiansen59@gmail.com>
2024-05-10 14:36:12 -0700

    FEATURE: get some movement of objects

--- src/graphics/snake.rs
+++ src/graphics/snake.rs
@@ -3,6 +3,7 @@
 use bevy::prelude::*;
 use bevy::render::color::Color;
 use bevy::sprite::{MaterialMesh2dBundle, Mesh2dHandle};
+use bevy::window::PrimaryWindow;
 
 
 #[derive(Component)]
@@ -20,29 +21,63 @@ struct Stats {
     health: i64
 }
 
+#[derive(Resource)]
+struct Game {
+    game_objects: Vec<Entity>,
+}
+
+impl Game {
+    fn new() -> Self {
+        Self {
+            game_objects: Vec::new(),
+        }
+    }
+
+    fn add(&mut self, game_object: Entity) {
+        self.game_objects.push(game_object);
+    }
+
+    fn get_game_objects(&self) -> Vec<Entity> {
+        return self.game_objects.clone();
+    }
+
+    fn display_objects(&self) {
+        println!("Display game objects\n");
+        for e in self.game_objects.clone() {
+            println!("Entity ID: {:?}, Index: {}, ?: {:?}", e, e.index(),e.to_bits());
+        }
+    }
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .insert_resource(Game::new())
         .add_systems(Startup, (setup, add_people, add_monster))
         .add_systems(Update, (draw_cursor, (update_people, greet_people, show_monster).chain()))
+        .add_systems(Update, move_square_entity)
         .run();
 }
 
-fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>) {
+fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>,
+         mut materials: ResMut<Assets<ColorMaterial>>, mut game: ResMut<Game>) {
     commands.spawn(Camera2dBundle::default());
 
     let square = Mesh2dHandle(meshes.add(Rectangle::new(50.0, 50.0)));
 
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: square,
-        material: materials.add(Color::ORANGE),
+
+    let square_entity = commands.spawn(MaterialMesh2dBundle {
+        mesh: square.into(),
+        material: materials.add(Color::AQUAMARINE),
         transform: Transform::from_xyz(
             50.0,
             20.0,
             20.0,
         ),
         ..default()
-    });
+    }).id();
+    game.add(square_entity);
+    game.display_objects();
 
     let square = Mesh2dHandle(meshes.add(Rectangle::new(100.0, 100.0)));
     commands.spawn(MaterialMesh2dBundle {
@@ -108,6 +143,35 @@ fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials
     // todo: cursor tracking
 }
 
+fn move_object() {
+    //TODO: FINISH ME
+}
+
+fn move_square_entity(
+    mut commands: Commands,
+    mut query: Query<(Entity, &mut Transform)>,
+    game: Res<Game>,
+    windows:Query<&Window, With<PrimaryWindow>>
+) {
+
+    // TODO: make these constants
+    let window = windows.single();
+    let window_width = window.width();
+    let window_height = window.height();
+
+    let x_boundary = window_width / 2.0;
+    let y_boundary = window_height / 2.0;
+
+    for (entity, mut transform) in query.iter_mut() {
+        if game.game_objects.contains(&entity) {
+            let new_x  = transform.translation.x - 5.0;
+            if new_x > -x_boundary && new_x < x_boundary {
+                transform.translation.x = new_x;
+            }
+        }
+    }
+}
+
 fn draw_cursor( camera_query: Query<(&Camera, &GlobalTransform)>,
                  windows: Query<&Window>, mut gizmos: Gizmos, ) {
     let (camera, camera_transform) = camera_query.single();
@@ -127,7 +191,7 @@ fn add_people(mut commands: Commands) {
 
 fn greet_people(query: Query<&Name, With<Person>>) {
     for name in &query {
-        println!("Hello {} !", name.0);
+       //println!("Hello {} !", name.0);
     }
 }
 
@@ -146,6 +210,11 @@ fn add_monster(mut commands: Commands) {
 
 fn show_monster(query: Query<&Stats, With<Monster>>) {
     for monster in &query {
-        println!("RAWR!!!!!!! {}, with health: {}  !", monster.name, monster.health);
+        //println!("RAWR!!!!!!! {}, with health: {}  !", monster.name, monster.health);
     }
+}
+
+// transforms
+fn transform() {
+
 }
